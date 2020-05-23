7d5ca716acf476313f82a8fc5d6e1b68ddd9937e
LongBody <54003924+LongBody@users.noreply.github.com>
2020-05-23 23:10:15 +0700

    Updates

---
+++ precache-manifest.de2a8c759de0592275213c1715027ddf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a88d782db22ab50277e42c4c0f8fa052",
+    "url": "/phoneX-React/index.html"
+  },
+  {
+    "revision": "0195b3b4e2a0623d7d19",
+    "url": "/phoneX-React/static/css/main.8e0fd072.chunk.css"
+  },
+  {
+    "revision": "945f772843ddf9140079",
+    "url": "/phoneX-React/static/js/2.aa544c1c.chunk.js"
+  },
+  {
+    "revision": "a636ddd461bd633ab63e86040460d655",
+    "url": "/phoneX-React/static/js/2.aa544c1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0195b3b4e2a0623d7d19",
+    "url": "/phoneX-React/static/js/main.4e55d146.chunk.js"
+  },
+  {
+    "revision": "d1e17d2dda943c97c0e3",
+    "url": "/phoneX-React/static/js/runtime-main.31a80b22.js"
+  },
+  {
+    "revision": "c8c592bf8574bfe9f7a906debc8b6245",
+    "url": "/phoneX-React/static/media/phonexCollapse.c8c592bf.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/phoneX-React/precache-manifest.de2a8c759de0592275213c1715027ddf.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/phoneX-React/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
